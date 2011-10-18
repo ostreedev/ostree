6114bc3b065815c7810fa1ebf6defaf5bfc3459c
Javier Jardón <jjardon@gnome.org>
2011-10-18 17:35:19 +0100

    autogen.sh: Improve the script a bit
    Fix builddir != sourcedir issues ans also run ./configure if
    Its needed

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
+
+olddir=`pwd`
+cd $srcdir
+
+AUTORECONF=`which autoreconf`
+if test -z $AUTORECONF; then
+        echo "*** No autoreconf found, please intall it ***"
+        exit 1
+fi
+
 mkdir -p m4
 
-exec autoreconf -vfi
+autoreconf --force --install --verbose
+
+cd $olddir
+test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
