f4b6d8825414f4c78a9f672a07b59138a4d156f9
Colin Walters <walters@verbum.org>
2012-06-08 16:34:45 -0400

    ostbuild: Fix -k option to git-mirror

--- src/ostbuild/pyostbuild/vcs.py
+++ src/ostbuild/pyostbuild/vcs.py
@@ -144,9 +144,9 @@ def ensure_vcs_mirror(mirrordir, keytype, uri, branch):
 def fetch(mirrordir, keytype, uri, branch, keep_going=False):
     mirror = buildutil.get_mirrordir(mirrordir, keytype, uri)
     last_fetch_path = get_lastfetch_path(mirrordir, keytype, uri, branch)
-    run_sync(['git', 'fetch'], cwd=mirror, log_initiation=False) 
-    current_vcs_version = run_sync_get_output(['git', 'rev-parse', branch], cwd=mirror,
-                                              none_on_error=keep_going)
+    run_sync(['git', 'fetch'], cwd=mirror, log_initiation=False,
+             none_on_error=keep_going) 
+    current_vcs_version = run_sync_get_output(['git', 'rev-parse', branch], cwd=mirror)
     if current_vcs_version is not None:
         current_vcs_version = current_vcs_version.strip()
         f = open(last_fetch_path, 'w')
