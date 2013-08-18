b35840e1b8b1f5c87b999bbe4bd460de327a05b9
Colin Walters <walters@verbum.org>
2013-08-18 07:50:50 -0400

    libotutil: Drop accidental use of GLib 2.36 API
    Sticking with 2.34 for a while longer.

--- src/libotutil/ot-variant-utils.c
+++ src/libotutil/ot-variant-utils.c
@@ -44,7 +44,12 @@ ot_gvariant_new_bytearray (const guchar   *data,
 GVariant *
 ot_gvariant_new_ay_bytes (GBytes *bytes)
 {
-  return g_variant_new_from_bytes (G_VARIANT_TYPE ("ay"), bytes, TRUE);
+  gsize size;
+  gconstpointer data;
+  data = g_bytes_get_data (bytes, &size);
+  g_bytes_ref (bytes);
+  return g_variant_new_from_data (G_VARIANT_TYPE ("ay"), data, size,
+                                  TRUE, (GDestroyNotify)g_bytes_unref, bytes);
 }
 
 GHashTable *
