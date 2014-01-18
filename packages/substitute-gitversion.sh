b38fb802f9a21fd59a731a6d77bf650c8e7c6d15
Colin Walters <walters@verbum.org>
2014-01-18 04:44:54 -0500

    packaging: Update infrastructure
    The Makefile.dist-packaging lives canonically in rpm-ostree/ for now,
    it's my latest hack to automate git -> (s)rpm.
    
    Update the spec.in from current Fedora.

--- packages/substitute-gitversion.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-git_version=$(git describe)
-git_version_rpm=$(echo ${git_version} | sed -e 's,-,\.,g' -e 's,^v,,')
-exec sed -e "s,^Version:.*,Version: ${git_version_rpm}," "$@"
