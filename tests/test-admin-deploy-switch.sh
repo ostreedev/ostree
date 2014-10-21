4a75286d84f5449b055059a95034facec01308f7
Giuseppe Scrivano <gscrivan@redhat.com>
2014-10-21 21:08:15 +0200

    tests: test-admin-deploy-switch doesn't use deprecated "current" symlink
    commit dfeb27eca55d923c57735e491e438ae54f8cc201 removed it, so change
    the test to not use it.
    
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- tests/test-admin-deploy-switch.sh
+++ tests/test-admin-deploy-switch.sh
@@ -32,14 +32,16 @@ echo "1..3"
 ostree --repo=sysroot/ostree/repo remote add --set=gpg-verify=false testos file://$(pwd)/testos-repo
 ostree --repo=sysroot/ostree/repo pull testos testos/buildmaster/x86_64-runtime
 ostree admin --sysroot=sysroot deploy --karg=root=LABEL=MOO --karg=quiet --os=testos testos:testos/buildmaster/x86_64-runtime
-assert_not_has_file sysroot/ostree/deploy/testos/current/usr/include/foo.h
+newrev=$(ostree --repo=sysroot/ostree/repo rev-parse testos:testos/buildmaster/x86_64-runtime)
+assert_not_has_file sysroot/ostree/deploy/testos/deploy/$newrev.0/usr/include/foo.h
 if ostree admin --sysroot=sysroot switch --os=testos testos/buildmaster/x86_64-runtime; then
     assert_not_reached "Switch to same ref unexpectedly succeeded"
 fi
 echo "ok switch expected error"
 
 ostree admin --sysroot=sysroot switch --os=testos testos/buildmaster/x86_64-devel
-assert_file_has_content sysroot/ostree/deploy/testos/current/usr/include/foo.h 'header'
+newrev=$(ostree --repo=sysroot/ostree/repo rev-parse testos:testos/buildmaster/x86_64-devel)
+assert_file_has_content sysroot/ostree/deploy/testos/deploy/$newrev.0/usr/include/foo.h 'header'
 
 echo "ok switch"
 
