479b5ab4fcdda8596c8c64e52b01537e49a6070a
Giuseppe Scrivano <gscrivan@redhat.com>
2015-03-06 11:48:24 +0100

    libotutil: remove ot-waitable-queue.
    The module is not not used anymore.  It can be restored from git if
    needed again.
    
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- src/libotutil/otutil.h
+++ src/libotutil/otutil.h
@@ -38,7 +38,6 @@
     }                                                      \
   } G_STMT_END;
 
-#include <ot-waitable-queue.h>
 #include <ot-keyfile-utils.h>
 #include <ot-gio-utils.h>
 #include <ot-fs-utils.h>
