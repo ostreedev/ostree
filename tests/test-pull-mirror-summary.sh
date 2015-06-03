0bd10eb6e23625a7f468dc1d89c3c17d9d428506
Giuseppe Scrivano <gscrivan@redhat.com>
2015-06-03 12:55:30 +0200

    tests: add test for check for remote add --set=gpg-verify-summary=true
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- tests/test-pull-mirror-summary.sh
+++ tests/test-pull-mirror-summary.sh
@@ -63,6 +63,13 @@ rm -rf repo
 mkdir repo
 ${OSTREE} --repo=repo init --mode=archive-z2
 ${OSTREE} --repo=repo remote add origin $(cat httpd-address)/ostree/gnomerepo
+echo "ok pull mirror without checking signed summary"
+
+cd ${test_tmpdir}
+rm -rf repo
+mkdir repo
+${OSTREE} --repo=repo init --mode=archive-z2
+${OSTREE} --repo=repo remote add --set=gpg-verify-summary=true origin $(cat httpd-address)/ostree/gnomerepo
 ${OSTREE} --repo=repo pull --mirror origin 2>/dev/null || touch expected-fail
 assert_has_file expected-fail
 echo "ok pull mirror without signed summary"
@@ -73,7 +80,7 @@ cd ${test_tmpdir}
 rm -rf repo
 mkdir repo
 ${OSTREE} --repo=repo init --mode=archive-z2
-${OSTREE} --repo=repo remote add origin $(cat httpd-address)/ostree/gnomerepo
+${OSTREE} --repo=repo remote add --set=gpg-verify-summary=true origin $(cat httpd-address)/ostree/gnomerepo
 ${OSTREE} --repo=repo pull --mirror origin
 assert_has_file repo/summary
 assert_has_file repo/summary.sig
