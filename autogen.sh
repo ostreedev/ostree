f0a02fbf20ebf4b1f2e248fee24a0babb2b22c03
Jeremy Cline <jeremy@jcline.org>
2015-06-29 13:20:57 -0400

    build: Make gtk-doc optional
    This is similar to what's done in glib.

--- autogen.sh
+++ autogen.sh
@@ -16,8 +16,8 @@ set -e
 
 mkdir -p m4
 
-GTKDOCIZE=$(which gtkdocize 2>/dev/null)
-if test -z $GTKDOCIZE; then
+GTKDOCIZE=$(which gtkdocize 2>/dev/null || true)
+if test -z "$GTKDOCIZE"; then
         echo "You don't have gtk-doc installed, and thus won't be able to generate the documentation."
         rm -f gtk-doc.make
         cat > gtk-doc.make <<EOF
@@ -25,7 +25,7 @@ EXTRA_DIST =
 CLEANFILES =
 EOF
 else
-        gtkdocize || exit $?
+        gtkdocize
 fi
 
 cd $olddir
