422de2ecef64dfbb65524ea30698e91bdb115b42
Giuseppe Scrivano <gscrivan@redhat.com>
2015-11-12 15:19:14 +0100

    fixup! tests: add tests for prune --before=DATE

--- tests/test-prune.sh
+++ tests/test-prune.sh
@@ -85,11 +85,11 @@ assert_file_has_content tombstonecommitcount "^1$"
 ${CMD_PREFIX} ostree prune --repo=repo --refs-only --depth=0 -v
 find repo/objects -name '*.commit' | wc -l > commitcount
 assert_file_has_content commitcount "^1$"
-${CMD_PREFIX} ostree --repo=repo commit --branch=test -m test -s test tree --timestamp="October 25 1985"
-${CMD_PREFIX} ostree --repo=repo commit --branch=test -m test -s test tree --timestamp="October 21 2015"
+${CMD_PREFIX} ostree --repo=repo commit --branch=test -m test -s test tree --timestamp="2010-10-29 12:43:29 +0000"
+${CMD_PREFIX} ostree --repo=repo commit --branch=test -m test -s test tree --timestamp="2005-10-29 12:43:29 +0000"
 find repo/objects -name '*.commit' | wc -l > commitcount
 assert_file_has_content commitcount "^3$"
-${CMD_PREFIX} ostree --repo=repo prune --before="1 week ago"
+${CMD_PREFIX} ostree --repo=repo prune --before="2015-10-29 12:43:29 +0000"
 find repo/objects -name '*.commit' | wc -l > commitcount
 assert_file_has_content commitcount "^1$"
 
