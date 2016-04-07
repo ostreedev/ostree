9d59f15c071d25db8c0e3a05af703d351217033b
Giuseppe Scrivano <gscrivan@redhat.com>
2016-04-07 10:11:11 +0200

    contrib: indent golang code using only tabs instead of both tabs and spaces
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- contrib/golang/ostree_test.go
+++ contrib/golang/ostree_test.go
@@ -9,47 +9,47 @@ import (
 )
 
 func TestTypeName(t *testing.T) {
-     name := RepoGetType().Name();
-     if name != "OstreeRepo" {
-     	t.Errorf("%s != OstreeRepo");
-     }
+	name := RepoGetType().Name();
+	if name != "OstreeRepo" {
+		t.Errorf("%s != OstreeRepo");
+	}
 }
 
 func TestRepoNew(t *testing.T) {
-     r, err := RepoNewOpen("/ostree/repo")
-     if err != nil {
-     	t.Errorf("%s", err);
-	return
-     }
-     parent := r.GetParent()
-     if parent != nil {
-     	t.Errorf("Expected no parent")
-	return
-     }
+	r, err := RepoNewOpen("/ostree/repo")
+	if err != nil {
+		t.Errorf("%s", err);
+		return
+	}
+	parent := r.GetParent()
+	if parent != nil {
+		t.Errorf("Expected no parent")
+		return
+	}
 }
 
 func TestRepoGetMetadataVersion(t *testing.T) {
-     r, err := RepoNewOpen("/ostree/repo")
-     if err != nil {
-     	t.Errorf("%s", err);
-	return
-     }
-     commit,err := r.ResolveRev("rhel-atomic-host/7/x86_64/standard")
-     if err != nil {
-     	t.Errorf("%s", err)
-	return
-     }
-     commitv,err := r.LoadVariant(OBJECT_TYPE_COMMIT, commit)
-     if err != nil {
-     	t.Errorf("%s", err)
-	return
-     }
-     ver, err := commitv.CommitGetMetadataKeyString("version")
-     if err != nil {
-     	t.Errorf("%s", err)
-	return
-     }
-     if ver != "7.1.3" {
-     	t.Errorf("expected 7.1.3")
-     }
+	r, err := RepoNewOpen("/ostree/repo")
+	if err != nil {
+		t.Errorf("%s", err);
+		return
+	}
+	commit,err := r.ResolveRev("rhel-atomic-host/7/x86_64/standard")
+	if err != nil {
+		t.Errorf("%s", err)
+		return
+	}
+	commitv,err := r.LoadVariant(OBJECT_TYPE_COMMIT, commit)
+	if err != nil {
+		t.Errorf("%s", err)
+		return
+	}
+	ver, err := commitv.CommitGetMetadataKeyString("version")
+	if err != nil {
+		t.Errorf("%s", err)
+		return
+	}
+	if ver != "7.1.3" {
+		t.Errorf("expected 7.1.3")
+	}
 }
