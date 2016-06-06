7fb49037ab6033bc53fa2eb3b516d7709b558d90
Colin Walters <walters@verbum.org>
2016-06-06 14:49:11 -0400

    tests/test-pull-c: New test that runs through the pull API via C
    We have had in the past issues with running `ostree_repo_pull()`
    multiple times in the same process, embarassingly enough.  Nothing in
    the current test suite covers this, so let's start.
    
    Closes: #322
    Approved by: jlebon

--- tests/libostreetest.c
+++ tests/libostreetest.c
@@ -28,8 +28,8 @@
 /* This function hovers in a quantum superposition of horrifying and
  * beautiful.  Future generations may interpret it as modern art.
  */
-static gboolean
-run_libtest (const char *cmd, GError **error)
+gboolean
+ot_test_run_libtest (const char *cmd, GError **error)
 {
   gboolean ret = FALSE;
   const char *builddir = g_getenv ("G_TEST_BUILDDIR");
@@ -68,7 +68,7 @@ ot_test_setup_repo (GCancellable *cancellable,
   g_autoptr(GFile) repo_path = g_file_new_for_path ("repo");
   glnx_unref_object OstreeRepo* ret_repo = NULL;
 
-  if (!run_libtest ("setup_test_repository archive-z2", error))
+  if (!ot_test_run_libtest ("setup_test_repository archive-z2", error))
     goto out;
 
   ret_repo = ostree_repo_new (repo_path);
@@ -91,7 +91,7 @@ ot_test_setup_sysroot (GCancellable *cancellable,
   g_autoptr(GFile) sysroot_path = g_file_new_for_path ("sysroot");
   glnx_unref_object OstreeSysroot *ret_sysroot = NULL;
 
-  if (!run_libtest ("setup_os_repository \"archive-z2\" \"syslinux\"", error))
+  if (!ot_test_run_libtest ("setup_os_repository \"archive-z2\" \"syslinux\"", error))
     goto out;
 
   ret_sysroot = ostree_sysroot_new (sysroot_path);
