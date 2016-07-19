97d4eca1ea2646c7d870ff17db3c98edc2b7427b
William Manley <will@williammanley.net>
2016-07-19 03:14:26 +0100

    ostree commit: Fix combining trees with multiple --tree=ref arguments
    You'd expect
    
        ostree commit --tree=ref=A --tree=ref=B
    
    to produce a commit with the union of the trees given.  Instead you'd get
    a commit with the contents of just the latter commit.  This was due to an
    optimisation where we'd skip filling out the `files` and `subdirs`
    members of the mtree, just filling in the metadata instead.  This backfires
    becuase this same code relies on checking the `files` and `subdirs` members
    itself to work out whether the mtree is empty.
    
    This commit removes the optimisation, fixing the bug.  Maybe there's a way
    to keep the optimisation and still fix the bug but it's not obvious to
    me.

---
+++ tests/test-commit.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 William Manley <will@williammanley.net>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+
+set -euo pipefail
+
+. $(dirname $0)/libtest.sh
+
+skip_without_user_xattrs
+
+echo "1..1"
+
+setup_test_repository "bare"
+
+$OSTREE commit --tree=dir=$test_srcdir/tree-A -b A -m A
+$OSTREE commit --tree=dir=$test_srcdir/tree-B -b B -m B
+
+$OSTREE commit --tree=ref=A --tree=ref=B -b Combine -m Combine
+$OSTREE ls Combine / >contents
+assert_file_has_content contents "/file-a"
+assert_file_has_content contents "/file-b"
+echo "ok combining ref trees"
