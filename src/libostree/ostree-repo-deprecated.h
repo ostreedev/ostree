cf54e772b594d7c83735472a9d827ac4257bef18
Giuseppe Scrivano <gscrivan@redhat.com>
2016-07-29 16:19:49 +0200

    fixup! libostree: mark ostree_repo_checkout_tree_at as deprecated

--- src/libostree/ostree-repo-deprecated.h
+++ src/libostree/ostree-repo-deprecated.h
@@ -1,6 +1,6 @@
 /* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*-
  *
- * Copyright (C) 2011 Colin Walters <walters@verbum.org>
+ * Copyright (C) 2016 Red Hat, Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -16,8 +16,6 @@
  * License along with this library; if not, write to the
  * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
- *
- * Author: Colin Walters <walters@verbum.org>
  */
 
 #pragma once
