ba97f3e58a842a45f79157260c443a71ed54632c
Jonathan Lebon <jlebon@redhat.com>
2016-10-14 11:05:28 -0400

    fixup! OstreeFetcher: provide proxy credentials if needed

--- src/libostree/ostree-fetcher.c
+++ src/libostree/ostree-fetcher.c
@@ -65,6 +65,9 @@ typedef struct {
 
   /* Also protected by output_stream_set_lock. */
   guint64 total_downloaded;
+
+  GError *oob_error;
+
 } ThreadClosure;
 
 static void
@@ -158,6 +161,8 @@ thread_closure_unref (ThreadClosure *thread_closure)
 
       g_clear_pointer (&thread_closure->output_stream_set, g_hash_table_unref);
       g_mutex_clear (&thread_closure->output_stream_set_lock);
+
+      g_clear_pointer (&thread_closure->oob_error, g_error_free);
 
       g_slice_free (ThreadClosure, thread_closure);
     }
@@ -280,6 +285,8 @@ static void
 on_authenticate (SoupSession *session, SoupMessage *msg, SoupAuth *auth,
                  gboolean retrying, gpointer user_data)
 {
+  ThreadClosure *thread_closure = user_data;
+
   if (msg->status_code == SOUP_STATUS_PROXY_UNAUTHORIZED)
     {
       SoupURI *uri = NULL;
@@ -287,7 +294,9 @@ on_authenticate (SoupSession *session, SoupMessage *msg, SoupAuth *auth,
       if (retrying)
         {
           g_autofree char *s = soup_uri_to_string (uri, FALSE);
-          g_warning ("Invalid username or password for proxy '%s'", s);
+          g_set_error (&thread_closure->oob_error,
+                       G_IO_ERROR, G_IO_ERROR_PROXY_AUTH_FAILED,
+                       "Invalid username or password for proxy '%s'", s);
         }
       else
         soup_auth_authenticate (auth, soup_uri_get_user (uri),
@@ -313,7 +322,7 @@ session_thread_set_proxy_cb (ThreadClosure *thread_closure,
       soup_uri_get_password (proxy_uri))
     {
       g_signal_connect (thread_closure->session, "authenticate",
-                        G_CALLBACK (on_authenticate), NULL);
+                        G_CALLBACK (on_authenticate), thread_closure);
     }
 }
 
@@ -1028,10 +1037,23 @@ on_request_sent (GObject        *object,
                   code = G_IO_ERROR_FAILED;
                 }
 
-              local_error = g_error_new (G_IO_ERROR, code,
-                                         "Server returned status %u: %s",
-                                         msg->status_code,
-                                         soup_status_get_phrase (msg->status_code));
+              {
+                g_autofree char *errmsg =
+                  g_strdup_printf ("Server returned status %u: %s",
+                                   msg->status_code,
+                                   soup_status_get_phrase (msg->status_code));
+
+                /* Let's make OOB errors be the final one since they're probably
+                 * the cause for the error here. */
+                if (pending->thread_closure->oob_error)
+                  {
+                    local_error =
+                      g_error_copy (pending->thread_closure->oob_error);
+                    g_prefix_error (&local_error, "%s: ", errmsg);
+                  }
+                else
+                  local_error = g_error_new_literal (G_IO_ERROR, code, errmsg);
+              }
 
               if (pending->mirrorlist->len > 1)
                 g_prefix_error (&local_error,
