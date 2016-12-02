c66bdc2e5f00b48b5e0d041da48256de6ccdc7ee
Alexander Larsson <alexl@redhat.com>
2016-12-02 13:49:51 +0100

    ostree-fetcher: Add g_autoptr support

--- src/libostree/ostree-fetcher.h
+++ src/libostree/ostree-fetcher.h
@@ -47,6 +47,8 @@ struct OstreeFetcherClass
   GObjectClass parent_class;
 };
 
+G_DEFINE_AUTOPTR_CLEANUP_FUNC (OstreeFetcher, g_object_unref)
+
 typedef enum {
   OSTREE_FETCHER_FLAGS_NONE = 0,
   OSTREE_FETCHER_FLAGS_TLS_PERMISSIVE = (1 << 0)
