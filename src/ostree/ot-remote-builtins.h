7f2830cb4e234c89a5aeaf600717d291ec09a6c0
Colin Walters <walters@verbum.org>
2016-12-07 21:16:52 -0500

    build: Make libsoup optional again
    The "remote cookies" code broke this.  While I'm not sure anyone is
    actually using ostree-without-http, it isn't too hard to keep the
    build time conditional going.  Further, this work is preparatory for
    libcurl porting.
    
    Closes: #621
    Approved by: jlebon

--- src/ostree/ot-remote-builtins.h
+++ src/ostree/ot-remote-builtins.h
@@ -25,12 +25,14 @@
 G_BEGIN_DECLS
 
 gboolean ot_remote_builtin_add (int argc, char **argv, GCancellable *cancellable, GError **error);
-gboolean ot_remote_builtin_add_cookie (int argc, char **argv, GCancellable *cancellable, GError **error);
 gboolean ot_remote_builtin_delete (int argc, char **argv, GCancellable *cancellable, GError **error);
-gboolean ot_remote_builtin_delete_cookie (int argc, char **argv, GCancellable *cancellable, GError **error);
 gboolean ot_remote_builtin_gpg_import (int argc, char **argv, GCancellable *cancellable, GError **error);
 gboolean ot_remote_builtin_list (int argc, char **argv, GCancellable *cancellable, GError **error);
+#ifdef HAVE_LIBSOUP
+gboolean ot_remote_builtin_add_cookie (int argc, char **argv, GCancellable *cancellable, GError **error);
 gboolean ot_remote_builtin_list_cookies (int argc, char **argv, GCancellable *cancellable, GError **error);
+gboolean ot_remote_builtin_delete_cookie (int argc, char **argv, GCancellable *cancellable, GError **error);
+#endif
 gboolean ot_remote_builtin_show_url (int argc, char **argv, GCancellable *cancellable, GError **error);
 gboolean ot_remote_builtin_refs (int argc, char **argv, GCancellable *cancellable, GError **error);
 gboolean ot_remote_builtin_summary (int argc, char **argv, GCancellable *cancellable, GError **error);
