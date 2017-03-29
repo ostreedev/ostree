50feb7e5344ae0310c97e6358088778d58e2d9cf
Colin Walters <walters@verbum.org>
2017-03-28 22:37:38 -0400

    libutil: Delete unused threadpool wrapper
    This is dead code since 9cc98041953090160dde48afa69b97c936541cdb where
    pull-local became just a wrapper for pull, which has its own threading.

--- src/libotutil/otutil.h
+++ src/libotutil/otutil.h
@@ -49,7 +49,6 @@
 #include <ot-opt-utils.h>
 #include <ot-unix-utils.h>
 #include <ot-variant-utils.h>
-#include <ot-spawn-utils.h>
 #include <ot-checksum-utils.h>
 #include <ot-gpg-utils.h>
 #include <ot-log-utils.h>
