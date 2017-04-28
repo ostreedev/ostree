54e7e6da864dc03a5bbb9a1fe7f0a429a2cc8d9c
Colin Walters <walters@verbum.org>
2017-04-28 09:32:35 -0400

    ci: Add a context for testing flatpak
    This was part of the philosophy behind https://wiki.gnome.org/Initiatives/GnomeGoals/InstalledTests -
    libraries like libostree don't need to replicate everything in unit tests, we
    can use the tests from our dependencies directly too.
    
    We'll also get API break coverage testing too.

---
+++ ci/flatpak.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Build and run flatpak's unit tests using the just-built ostree for this PR.
+
+set -xeuo pipefail
+
+build() {
+    env NOCONFIGURE=1./autogen.sh
+    ./configure --prefix=/usr --libdir=/usr/lib64 "$@"
+    make -j 8
+}
+
+# build+install ostree
+build
+sudo make install
+tmpd=$(mktemp -d)
+cd ${tmpd}
+# Frozen to a tag for now on general principle
+git clone --recursive --depth=1 -b 0.9.3 https://github.com/flatpak/flatpak
+cd flatpak
+dnf builddep -y flatpak
+build
+make -j 8 check
