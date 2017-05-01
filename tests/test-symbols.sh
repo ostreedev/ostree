2dbd8455ea87a08d41833c26059222b257583a3b
Philip Withnall <withnall@endlessm.com>
2017-05-01 10:59:33 +0100

    build: Add --enable-experimental-api configure option for unstable APIs
    There are currently no unstable APIs, but some will be added in
    following commits. They will be built and exposed in the libostree
    global symbol list iff configured with --enable-experimental-api.
    
    Distributions should not package OSTree with --enable-experimental-api.
    This is designed for previewing new APIs on controlled platforms; any of
    the APIs hidden behind this option may be changed or removed at any
    point.
    
    Signed-off-by: Philip Withnall <withnall@endlessm.com>

--- tests/test-symbols.sh
+++ tests/test-symbols.sh
@@ -21,9 +21,17 @@ set -euo pipefail
 
 echo '1..2'
 
+if echo "$OSTREE_FEATURES" | grep --quiet --no-messages "experimental"; then
+  experimental_sym="${G_TEST_SRCDIR}/src/libostree/libostree-experimental.sym"
+  experimental_sections="${G_TEST_SRCDIR}/apidoc/ostree-experimental-sections.txt"
+else
+  experimental_sym=""
+  experimental_sections=""
+fi
+
 echo "Verifying all expected symbols are actually exported..."
-grep ' ostree_[A-Za-z0-9_]*;' ${G_TEST_SRCDIR}/src/libostree/libostree.sym | sed -e 's,^ *\([A-Za-z0-9_]*\);,\1,' | sort -u > expected-symbols.txt
-eu-readelf -a ${G_TEST_BUILDDIR}/.libs/libostree-1.so | grep 'FUNC.*GLOBAL.*DEFAULT.*@@LIBOSTREE_' | sed -e 's,^.* \(ostree_[A-Za-z0-9_]*\)@@LIBOSTREE_[0-9_.]*,\1,' |sort -u > found-symbols.txt
+grep --no-filename ' ostree_[A-Za-z0-9_]*;' ${G_TEST_SRCDIR}/src/libostree/libostree.sym $experimental_sym | sed -e 's,^ *\([A-Za-z0-9_]*\);,\1,' | sort -u > expected-symbols.txt
+eu-readelf -a ${G_TEST_BUILDDIR}/.libs/libostree-1.so | grep 'FUNC.*GLOBAL.*DEFAULT.*@@LIBOSTREE_' | sed -e 's,^.* \(ostree_[A-Za-z0-9_]*\)@@LIBOSTREE_[0-9A-Z_.]*,\1,' |sort -u > found-symbols.txt
 diff -u expected-symbols.txt found-symbols.txt
 echo "ok exports"
 
@@ -31,7 +39,7 @@ echo "ok exports"
 grep -E -v '(ostree_cmd__private__)|(ostree_fetcher_config_flags_get_type)' found-symbols.txt > expected-documented.txt
 
 echo "Verifying all public symbols are documented:"
-grep '^ostree_' ${G_TEST_SRCDIR}/apidoc/ostree-sections.txt |sort -u > found-documented.txt
+grep '^ostree_' ${G_TEST_SRCDIR}/apidoc/ostree-sections.txt $experimental_sections |sort -u > found-documented.txt
 diff -u expected-documented.txt found-documented.txt
 
 echo 'ok documented symbols'
