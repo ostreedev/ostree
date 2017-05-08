6d43a49390d79d6f74643a57ac0f3ff617ebe481
Colin Walters <walters@verbum.org>
2017-05-08 13:37:50 -0400

    tests: Migrate test-pull-many.sh to installed on FAH
    `test-pull-many.sh` is was just too slow to be a unit test.  Generating
    a bunch of files via shell is slow, the delta generation is slow, etc.
    Every developer doesn't need to run it every time.
    
    Somewhat address this by converting it into our installed test framework, which
    moves it out of the developer fast paths.  Another advantage to this is
    that we can simply reuse the FAH tree content rather than synthesizing
    new bits each time.

--- tests/installed/libinsttest.sh
+++ tests/installed/libinsttest.sh
@@ -20,6 +20,49 @@
 dn=$(dirname $0)
 . ${dn}/libtest-core.sh
 
+# Copy of bits from tap-test
+test_tmpdir=
+function _tmpdir_cleanup () {
+    if test -n "${test_tmpdir}" && test -f ${test_tmpdir}/.testtmp; then
+	      rm "${test_tmpdir}" -rf
+    fi
+}
+prepare_tmpdir() {
+    test_tmpdir=$(mktemp -d)
+    touch ${test_tmpdir}/.testtmp
+    cd ${test_tmpdir}
+    echo ${test_tmpdir}
+}
+
+# This is copied from flatpak/flatpak/tests/test-webserver.sh
+run_tmp_webserver() {
+    dir=$1
+
+    test -n ${test_tmpdir}
+
+    cd ${dir}
+    env PYTHONUNBUFFERED=1 setsid python -m SimpleHTTPServer 0 >${test_tmpdir}/httpd-output &
+    cd -
+    child_pid=$!
+
+    for x in $(seq 50); do
+        # Snapshot the output
+        cp ${test_tmpdir}/httpd-output{,.tmp}
+        # If it's non-empty, see whether it matches our regexp
+        if test -s ${test_tmpdir}/httpd-output.tmp; then
+            sed -e 's,Serving HTTP on 0.0.0.0 port \([0-9]*\) \.\.\.,\1,' < ${test_tmpdir}/httpd-output.tmp > ${test_tmpdir}/httpd-port
+            if ! cmp ${test_tmpdir}/httpd-output.tmp ${test_tmpdir}/httpd-port 1>/dev/null; then
+                # If so, we've successfully extracted the port
+                break
+            fi
+        fi
+        sleep 0.1
+    done
+    port=$(cat ${test_tmpdir}/httpd-port)
+    echo "http://127.0.0.1:${port}" > ${test_tmpdir}/httpd-address
+    echo "$child_pid" > ${test_tmpdir}/httpd-pid
+}
+
 # Determine our origin refspec - we'll use this as a test base
 rpmostree=$(which rpm-ostree 2>/dev/null)
 if test -z "${rpmostree}"; then
