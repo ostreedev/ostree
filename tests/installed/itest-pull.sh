0d0dcd55483eb5b643e9942ae3ea2144665acc78
Colin Walters <walters@verbum.org>
2017-05-08 15:01:26 -0400

    fixup! tests: Migrate test-pull-many.sh to installed on FAH

--- tests/installed/itest-pull.sh
+++ tests/installed/itest-pull.sh
@@ -16,10 +16,12 @@ rm repo bare-repo -rf
 mkdir repo
 ostree --repo=repo init --mode=archive
 echo -e '[archive]\nzlib-level=1\n' >> repo/config
-ostree --repo=repo pull-local /ostree/repo ${host_refspec}
+host_nonremoteref=$(echo ${host_refspec} | sed 's,[^:]*:,,')
+ostree --repo=repo pull-local /ostree/repo ${host_commit}
+ostree --repo=repo refs ${host_commit} --create=${host_nonremoteref}
 
 run_tmp_webserver $(pwd)/repo
 # Now test pulling via HTTP (no deltas) to a new bare-user repo
 ostree --repo=bare-repo init --mode=bare-user
 ostree --repo=bare-repo remote add origin --set=gpg-verify=false $(cat ${test_tmpdir}/httpd-address)
-ostree --repo=bare-repo pull --disable-static-deltas origin ${host_refspec}
+ostree --repo=bare-repo pull --disable-static-deltas origin ${host_nonremoteref}
