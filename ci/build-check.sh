a98faa911343378781114d458ba3936c282b90ec
Colin Walters <walters@verbum.org>
2017-06-08 13:47:20 -0400

    ci: Update to match current rpm-ostree
    This copies the `ci/` directory from rpm-ostree, with much the same rationale;
    among other things we don't want to depend on the Docker hub.
    
    The specific reason I'm doing this is that I want to add a CentOS7 build, but
    that means we can't use `projectatomic/ostree-tester`, and at that point we
    might as well unwind it all.
    
    Closes: #917
    Approved by: jlebon

---
+++ ci/build-check.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+# Install build dependencies, run unit tests and installed tests.
+
+set -xeuo pipefail
+
+dn=$(dirname $0)
+. ${dn}/libbuild.sh
+${dn}/build.sh
+make check
+make syntax-check  # TODO: do syntax-check under check
+# And now run the installed tests
+make install
+gnome-desktop-testing-runner -p 0 ostree
+
+git clean -dfx && git submodule foreach git clean -dfx
+# And now a clang build to find unused variables; perhaps
+# in the future these could parallelize
+export CC=clang
+export CFLAGS='-Werror=unused-variable'
+build
