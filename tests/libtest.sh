1f5f93c5362d79dea1335d4fb09634e4756c3da1
Colin Walters <walters@verbum.org>
2017-06-28 21:39:16 -0400

    tests: Fix assert_files_hardlinked
    It was always succeeding because we were trying to stat the inode number, and
    failing, and thus getting the empty string for both, which compared as true.
    
    Regression from:
    <https://github.com/ostreedev/ostree/commit/74e3581e>
    
    Noticed this while working on
    <https://github.com/ostreedev/ostree/pull/974>
    and looking at the test results.

--- tests/libtest.sh
+++ tests/libtest.sh
@@ -123,9 +123,7 @@ files_are_hardlinked() {
 }
 
 assert_files_hardlinked() {
-    f1=$(stat -c %i $1)
-    f2=$(stat -c %i $2)
-    if ! files_are_hardlinked "$f1" "$f2"; then
+    if ! files_are_hardlinked "$1" "$2"; then
         fatal "Files '$1' and '$2' are not hardlinked"
     fi
 }
