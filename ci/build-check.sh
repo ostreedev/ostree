e22a6bd5cebdef6b21cdf9f055ea825e141723be
Colin Walters <walters@verbum.org>
2017-07-19 09:21:23 -0400

    ci/papr: Add a suite to run introspection-based tests without ASAN
    Unfortunately we can't do gobject-introspection based tests
    while compiling with `-fsanitize=address`, since it needs to hook
    `malloc` early on.
    
    Add a new suite which just runs the introspection-based tests without ASAN.

--- ci/build-check.sh
+++ ci/build-check.sh
@@ -11,7 +11,7 @@ make syntax-check  # TODO: do syntax-check under check
 # And now run the installed tests
 make install
 if test -x /usr/bin/gnome-desktop-testing-runner; then
-    gnome-desktop-testing-runner -p 0 libostree/
+    gnome-desktop-testing-runner -p 0 libostree/ ${INSTALLED_TESTS_PATTERN:-}
 fi
 
 if test -x /usr/bin/clang; then
