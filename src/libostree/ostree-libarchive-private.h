49f55c7f2cc3c4ddb08e3fc984b2c9ada45c9f37
Colin Walters <walters@verbum.org>
2017-08-22 21:52:24 -0400

    libarchive: Add support for translating paths during commit
    For rpm-ostree, I want to move RPM files in `/boot` to `/usr/lib/ostree-boot`.
    This is currently impossible without forking the libarchive code.  Supporting
    this is pretty straightforward; we already had pathname translation in
    the libarchive code, we just need to expose it as an option.
    
    On the command line side, I chose to wrap this as a regexp. That should be good
    enough for a lot of use cases; sophisticated users should as always be making
    use of the API. Note that this required some new `#ifdef LIBARCHIVE` bits to use
    the new API. Following previous patterns here, we use the new API only if a
    relevant option is enabled, ensuring unit test coverage of both paths.
    
    For the test cases, I ended up changing the accounting to avoid having to
    multiply the test count.

--- src/libostree/ostree-libarchive-private.h
+++ src/libostree/ostree-libarchive-private.h
@@ -38,6 +38,28 @@ typedef struct archive OtAutoArchiveWrite;
 G_DEFINE_AUTOPTR_CLEANUP_FUNC(OtAutoArchiveWrite, archive_write_free)
 typedef struct archive OtAutoArchiveRead;
 G_DEFINE_AUTOPTR_CLEANUP_FUNC(OtAutoArchiveRead, archive_read_free)
+
+static inline OtAutoArchiveRead *
+ot_open_archive_read (const char *path, GError **error)
+{
+  g_autoptr(OtAutoArchiveRead) a = archive_read_new ();
+
+#ifdef HAVE_ARCHIVE_READ_SUPPORT_FILTER_ALL
+  archive_read_support_filter_all (a);
+#else
+  archive_read_support_compression_all (a);
+#endif
+  archive_read_support_format_all (a);
+  if (archive_read_open_filename (a, path, 8192) != ARCHIVE_OK)
+    {
+      g_set_error (error, G_IO_ERROR, G_IO_ERROR_FAILED,
+                   "%s", archive_error_string (a));
+      return NULL;
+    }
+
+  return g_steal_pointer (&a);
+}
+
 #endif
 
 G_END_DECLS
