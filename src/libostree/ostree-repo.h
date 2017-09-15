b64ab67879f9711f70c960670ff7953846f5d9b3
Philip Withnall <withnall@endlessm.com>
2017-09-15 16:03:36 +0100

    lib/repo: Drop outdated FIXME comment from ostree-repo.h
    This was some incomplete planning from while the find_remotes() API was
    being designed; now totally outdated.
    
    Signed-off-by: Philip Withnall <withnall@endlessm.com>

--- src/libostree/ostree-repo.h
+++ src/libostree/ostree-repo.h
@@ -1156,33 +1156,6 @@ ostree_repo_pull_one_dir (OstreeRepo               *self,
                           GCancellable             *cancellable,
                           GError                  **error);
 
-
-
-#if 0
-FIXME
-Called with: remote_name, refs, override-commit-ids
-or: URL, refs, override-commit-ids
-=> we only need refs; could use the remote_name or URL as additional results
-
-Summary file is downloaded first, so this would result in multiple downloads of
-the summary, but we don’t care because of caching.
-
-Big problem preventing this from being the overall API: presenting the download
-sizes in the gnome-software UI before the user chooses to download.
-
-_OSTREE_PUBLIC
-gboolean ostree_repo_find_remotes_squashed (OstreeRepo           *self,
-                                                   const gchar * const  *refs, -> options
-                                                   GVariant             *options,
-                                                   OstreeRepoFinder    **finders, -> options
-                                                   GMainContext         *context, -> nope
-                                                   OstreeAsyncProgress  *progress,
-                                                   GCancellable         *cancellable,
-                                                   GError              **error);
-#endif
-
-
-
 _OSTREE_PUBLIC
 gboolean ostree_repo_pull_with_options (OstreeRepo             *self,
                                         const char             *remote_name_or_baseurl,
