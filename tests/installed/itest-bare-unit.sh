4262a4b016ec1ca252dfb4b2fcc82a27f643ea63
Jonathan Lebon <jlebon@redhat.com>
2017-09-29 21:42:23 +0000

    tests/installed: also run test-basic-c
    Since we now have a subtest there that needs full xattr support.
    
    Closes: #1170
    Approved by: cgwalters

--- tests/installed/itest-bare-unit.sh
+++ tests/installed/itest-bare-unit.sh
@@ -8,6 +8,12 @@ set -xeuo pipefail
 dn=$(dirname $0)
 . ${dn}/libinsttest.sh
 
+# These tests sort of bypass the installed-tests spec;
+# fixing that would require installing g-d-t-r, though
+# more ideally we architect things with a "control" container
+# distinct from the host.
+export G_TEST_SRCDIR=$(realpath $dn/../..)
+
 # Use /var/tmp to hopefully use XFS + O_TMPFILE etc.
 tempdir=$(mktemp -d /var/tmp/tap-test.XXXXXX)
 touch ${tempdir}/.testtmp
@@ -18,8 +24,5 @@ function cleanup () {
 }
 trap cleanup EXIT
 cd ${tempdir}
-# This sort of bypasses the installed-tests spec;
-# fixing that would require installing g-d-t-r, though
-# more ideally we architect things with a "control" container
-# distinct from the host.
 /usr/libexec/installed-tests/libostree/test-basic.sh
+/usr/libexec/installed-tests/libostree/test-basic-c
