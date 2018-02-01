aa989e1a66abfe4504773b86ee2926031223cb3f
Jonathan Lebon <jlebon@redhat.com>
2018-02-01 22:07:36 +0000

    tests/installed: support TESTS filter
    Lifted from rpm-ostree. Makes iterating on a single test much faster.
    Example use:
    
        TESTS=label-selinux ./ostree/tests/installed/run.sh

--- tests/installed/run.sh
+++ tests/installed/run.sh
@@ -4,6 +4,14 @@ set -xeuo pipefail
 
 dn=$(dirname $0)
 for tn in ${dn}/itest-*.sh; do
+    if [ -n "${TESTS+ }" ]; then
+      tbn=$(basename "$tn" .sh)
+      tbn=" ${tbn#itest-} "
+      if [[ " $TESTS " != *$tbn* ]]; then
+        echo "Skipping: ${tn}"
+        continue
+      fi
+    fi
     echo Executing: ${tn}
     ${tn}
 done
