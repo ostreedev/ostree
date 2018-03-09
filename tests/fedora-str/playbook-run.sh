9fd3861b621d10b471af7677c54450cb66ac53e8
Colin Walters <walters@verbum.org>
2018-03-09 18:35:12 -0500

    fixup! tests/str: Rework invocation

---
+++ tests/fedora-str/playbook-run.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+# A thin wrapper for ansible-playbook which has a nice check for
+# TEST_SUBJECTS being set.
+set -xeuo pipefail
+
+dn=$(cd $(dirname $0) && pwd)
+cd ${dn}
+
+# https://fedoraproject.org/wiki/CI/Tests
+if test -z "${TEST_SUBJECTS:-}"; then
+    cat <<EOF
+
+error: TEST_SUBJECTS must be set; e.g.:
+
+  curl -Lo fedora-atomic-host.qcow2 'https://getfedora.org/atomic_qcow2_latest'
+  export TEST_SUBJECTS=\$(pwd)/fedora-atomic-host.qcow2
+
+If you're doing interactive development, we recommend caching the qcow2
+somewhere persistent.
+EOF
+    exit 1
+fi
+ls -al ${TEST_SUBJECTS}
+
+export ANSIBLE_INVENTORY=${ANSIBLE_INVENTORY:-$(test -e inventory && echo inventory || echo /usr/share/ansible/inventory)}
+ls -al /dev/kvm
+exec ansible-playbook --tags=atomic "$@"
