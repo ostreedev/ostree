2057a7584996042843b55080765fedfaea8a9e9c
Colin Walters <walters@verbum.org>
2018-04-30 09:46:29 -0400

    repo: Add checksum to error message opening unreadable object
    This would have debugged trying to do a pull as non-root from
    a sysroot repository.  See for example:
    https://github.com/ostreedev/ostree/issues/1562

--- tests/installed/nondestructive/itest-bare-unit.sh
+++ tests/installed/nondestructive/itest-bare-unit.sh
@@ -21,3 +21,24 @@ trap _tmpdir_cleanup EXIT
 /usr/libexec/installed-tests/libostree/test-basic.sh
 /usr/libexec/installed-tests/libostree/test-basic-c
 date
+
+# Test error message when opening a non-world-readable object
+# https://github.com/ostreedev/ostree/issues/1562
+rm repo -rf
+chmod a+rx .
+ostree --repo=repo init --mode=bare
+mkdir files
+touch files/unreadable
+chmod 0 files/unreadable
+ostree --repo=repo commit -b testbranch --tree=dir=files
+# We should be able to read as non-root due to CAP_DAC_OVERRIDE
+ostree --repo=repo ls testbranch >/dev/null
+cat >upriv.sh <<EOF
+#!/bin/bash
+set -xeuo pipefail
+ostree --repo=testclone
+EOF
+if setpriv --reuid bin --regid bin --clear-groups ostree --repo=repo cat testbranch /unreadable 2>err.txt; then
+    fatal "Listed unreadable object as non-root"
+fi
+assert_file_has_content err.txt "Opening content object.*openat: Permission denied"
