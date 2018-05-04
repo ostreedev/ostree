d3b5a58d058982aa18f13047d8e4af11e35ceb49
Colin Walters <walters@verbum.org>
2018-05-04 13:51:13 -0400

    tests: Fix installed tests more
    OK so I noticed that something was failing and we were missing
    `set -xeuo pipefail` in our shells.  That of course revealed
    the ansible tests didn't actually work - my only defense
    here is spending so much time fighting to get it through CI
    and trying something new.
    
    Anyways, to make the staged-deploy tests work we need a task
    that actually uses `rpm-ostree override` rather than `usroverlay`.
    
    Let's make this a bit saner and have a clean split between
    tests that are "shell-script+usroverlay" and "ansible+override".

--- tests/installed/run.sh
+++ tests/installed/run.sh
@@ -6,7 +6,7 @@ set -xeuo pipefail
 dn=$(cd $(dirname $0) && pwd)
 
 # TODO: parallelize this
-PLAYBOOKS=${PLAYBOOKS:-nondestructive.yml destructive.yml}
+PLAYBOOKS=${PLAYBOOKS:-nondestructive.yml destructive-ansible.yml destructive-unit.yml}
 for playbook in $PLAYBOOKS; do
     time ${dn}/playbook-run.sh -v ${dn}/${playbook}
 done
