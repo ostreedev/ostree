a3295ee584a107784c34fea8616140b1e6ecdf94
Colin Walters <walters@verbum.org>
2018-05-07 21:02:12 -0700

    ci: Drop str hotpatch
    The change we want is in the current Fedora repos, and git master
    is broken:
    
    > qemu-system-x86_64: -vnc :1: Failed to start VNC server: Failed to bind socket: Address already in use
    
     https://pagure.io/standard-test-roles/pull-request/186#comment-52440
    
    Closes: #1582
    Approved by: jlebon

--- tests/installed/provision.sh
+++ tests/installed/provision.sh
@@ -6,7 +6,3 @@ dn=$(dirname $0)
 
 pkg_upgrade
 pkg_install git rsync openssh-clients ansible standard-test-roles
-
-# "Hot patch" this to pick up https://pagure.io/standard-test-roles/pull-request/152
-# so we get parallelism
-cd /usr/share/ansible/inventory && curl -L -O https://pagure.io/standard-test-roles/raw/master/f/inventory/standard-inventory-qcow2
