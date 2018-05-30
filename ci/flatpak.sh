bf3525adcbdcafa67597c216ed4465d4bceadd54
Matthew Leeds <matthew.leeds@endlessm.com>
2018-05-29 19:09:07 -0700

    ci: Use master branch as flatpak tag
    Closes: #1607
    Approved by: jlebon

--- ci/flatpak.sh
+++ ci/flatpak.sh
@@ -3,38 +3,24 @@
 
 set -xeuo pipefail
 
-# Frozen to a tag for now to help predictability; it's
-# also useful to test building *older* versions since
-# that must work.
-FLATPAK_TAG=0.10.2.1
+FLATPAK_TAG=master
 
 dn=$(dirname $0)
 . ${dn}/libpaprci/libbuild.sh
 
 codedir=$(pwd)
 
-pkg_upgrade
-pkg_install_buildroot
-pkg_builddep ostree flatpak
-pkg_install gettext-devel # A new dependency
-# Copy of builddeps from build.sh in flatpak
-pkg_install sudo which attr fuse \
-            libubsan libasan libtsan \
-            elfutils git gettext-devel libappstream-glib-devel \
-            /usr/bin/{update-mime-database,update-desktop-database,gtk-update-icon-cache} \
-            hicolor-icon-theme
-pkg_install flatpak && rpm -e flatpak
-
 # Build and install ostree
 cd ${codedir}
-build
+ci/build.sh
 make install
+
+# Build flatpak
 tmpd=$(mktemp -d)
 cd ${tmpd}
-# Frozen to a tag for now on general principle
 git clone --recursive --depth=1 -b ${FLATPAK_TAG} https://github.com/flatpak/flatpak
 cd ${tmpd}/flatpak
-build
+ci/build.sh
 # We want to capture automake results from flatpak
 cleanup() {
     mv test-suite.log ${codedir} || true
