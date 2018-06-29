c844e41fa83c847c8eca23d53bbee6cd0e5e6f7a
Jonathan Lebon <jonathan@jlebon.com>
2018-06-29 14:34:46 -0400

    fixup! ci/flatpak: Fix to use built ostree version

--- ci/flatpak.sh
+++ ci/flatpak.sh
@@ -28,7 +28,6 @@ pkg_install sudo which attr fuse bison \
             libubsan libasan libtsan clang python2 \
             elfutils git gettext-devel libappstream-glib-devel hicolor-icon-theme \
             /usr/bin/{update-mime-database,update-desktop-database,gtk-update-icon-cache}
-pkg_install_testing ostree-devel ostree
 pkg_install_builddeps flatpak
 # Now install ostree over the package version
 cd ${codedir}
