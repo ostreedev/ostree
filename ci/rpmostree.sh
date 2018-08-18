57dbd176e99b657dba2b7450a5823d0b27c4ff38
Jonathan Lebon <jonathan@jlebon.com>
2018-08-17 21:50:14 -0400

    ci: Bump rpm-ostree tag we build for tests
    The latest rpm-ostree release no longer requires `python3-devel` so `dnf
    builddep` here is no longer pulling it in, subsequently causing issues
    when building an older rpm-ostree release. Let's just bump the release
    tag so we don't have to also start keeping track of older dependencies.
    
    Closes: #1708
    Approved by: cgwalters

--- ci/rpmostree.sh
+++ ci/rpmostree.sh
@@ -6,7 +6,7 @@ set -xeuo pipefail
 # Frozen to a tag for now to help predictability; it's
 # also useful to test building *older* versions since
 # that must work.
-RPMOSTREE_TAG=v2018.5
+RPMOSTREE_TAG=v2018.7
 
 dn=$(dirname $0)
 . ${dn}/libpaprci/libbuild.sh
@@ -48,4 +48,4 @@ if ! make vmsync; then
     fatal "vmsync failed"
 fi
 # Now run tests; just a subset ⊂ for now to avoid CI overload
-make vmcheck TESTS="basic layering-basic"
+make vmcheck TESTS="layering-basic-1 layering-basic-2"
