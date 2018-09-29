19fef715921538012301d7af7540db414800a429
Felix Krull <f_krull@gmx.de>
2018-09-29 19:14:14 +0200

    Add as much of Repo as easily possible

--- rust-bindings/rust/libostree/src/lib.rs
+++ rust-bindings/rust/libostree/src/lib.rs
@@ -1,11 +1,13 @@
 extern crate libostree_sys as ffi;
-
 extern crate glib_sys as glib_ffi;
 extern crate gobject_sys as gobject_ffi;
-
+extern crate gio_sys as gio_ffi;
 #[macro_use]
 extern crate glib;
 extern crate gio;
+extern crate libc;
+#[macro_use]
+extern crate bitflags;
 
 use glib::Error;
 
