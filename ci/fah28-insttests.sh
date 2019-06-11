404d153944e85275a2693a650ca9ba6311662af5
Robert Fairley <rfairley@redhat.com>
2019-06-11 16:38:16 -0400

    ci: Use Fedora 29 artifacts
    Use Fedora 29 artifacts instead of Fedora 28, since 28 is now
    end-of-life.
    
    Also rename `ci/fah28-insttests.sh` -> `ci/fah29-insttests.sh`
    and use the https://getfedora.org/atomic_qcow2_latest redirect
    URL for the latest Fedora Atomic Host 29 image.

--- ci/fah28-insttests.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-set -xeuo pipefail
-
-./tests/installed/provision.sh
-# TODO: enhance papr to have caching, a bit like https://docs.travis-ci.com/user/caching/
-cd tests/installed
-# This should be https://getfedora.org/atomic_qcow2_latest but that's broken
-curl -Lo fedora-atomic-host.qcow2 https://kojipkgs.fedoraproject.org/compose/twoweek/Fedora-Atomic-28-20180626.0/compose/AtomicHost/x86_64/images/Fedora-AtomicHost-28-20180626.0.x86_64.qcow2
-exec env "TEST_SUBJECTS=$(pwd)/fedora-atomic-host.qcow2" ./run.sh
