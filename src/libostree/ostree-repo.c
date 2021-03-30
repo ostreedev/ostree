57270db9a1c75daa4ec11ab9f1bb41609c29214b
Colin Walters <walters@verbum.org>
2021-03-30 20:51:51 +0000

    repo: Ensure load_variant_if_exists sets NULL value
    Spotted by @jlebon - we usually expect callers to zero, but
    let's be explicit.

--- src/libostree/ostree-repo.c
+++ src/libostree/ostree-repo.c
@@ -3686,6 +3686,10 @@ load_metadata_internal (OstreeRepo       *self,
   g_return_val_if_fail (OSTREE_OBJECT_TYPE_IS_META (objtype), FALSE);
   g_return_val_if_fail (objtype == OSTREE_OBJECT_TYPE_COMMIT || out_state == NULL, FALSE);
 
+  /* Ensure this is set to NULL if we didn't find the object */
+  if (out_variant)
+    *out_variant = NULL;
+
   /* Special caching for dirmeta objects, since they're commonly referenced many
    * times.
    */
