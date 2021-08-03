709b35bf1159004c0c245c849d76cc7b92b16f76
Colin Walters <walters@verbum.org>
2021-08-03 10:52:21 -0400

    Fix build with --features=v2021_3, use in CI by default
    It's a huge trap for us not to build with the latest ostree feature
    on, I didn't have my IDE configured for it, and CI didn't have
    it on.
    
    The previous bump to glib 0.14 broke the Sign code.

--- rust-bindings/rust/tests/sign/mod.rs
+++ rust-bindings/rust/tests/sign/mod.rs
@@ -1,18 +1,21 @@
-use gio::NONE_CANCELLABLE;
-use glib::{Bytes, Variant};
-use ostree::{prelude::*, Sign};
+use ostree::prelude::*;
+use ostree::{gio, glib};
 
 #[test]
 fn sign_api_should_work() {
-    let dummy_sign = Sign::get_by_name("dummy").unwrap();
-    assert_eq!(dummy_sign.get_name().unwrap(), "dummy");
+    let dummy_sign = ostree::Sign::by_name("dummy").unwrap();
+    assert_eq!(dummy_sign.name().unwrap(), "dummy");
 
-    let result = dummy_sign.data(&Bytes::from_static(b"1234"), NONE_CANCELLABLE);
+    let result = ostree::prelude::SignExt::data(
+        &dummy_sign,
+        &glib::Bytes::from_static(b"1234"),
+        gio::NONE_CANCELLABLE,
+    );
     assert!(result.is_err());
 
-    let result = dummy_sign.data_verify(&Bytes::from_static(b"1234"), &Variant::from("1234"));
+    let result = dummy_sign.data_verify(&glib::Bytes::from_static(b"1234"), &"1234".to_variant());
     assert!(result.is_err());
 
-    let result = Sign::get_by_name("NOPE");
+    let result = ostree::Sign::by_name("NOPE");
     assert!(result.is_err());
 }
