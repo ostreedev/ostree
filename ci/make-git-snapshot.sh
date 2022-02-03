92c396c82bb2a4041bac7f5d14c77f7d8c0bf23a
Jonathan Lebon <jonathan@jlebon.com>
2022-02-03 13:22:47 -0500

    ci/make-git-snapshot.sh: auto-initialize submodules
    Matches `autogen.sh`.

--- ci/make-git-snapshot.sh
+++ ci/make-git-snapshot.sh
@@ -11,6 +11,10 @@ PKG_VER="${name}-${version}"
 TARFILE=${PKG_VER}.tar
 TARFILE_TMP=${TARFILE}.tmp
 
+if ! test -f ${TOP}/libglnx/README.md || ! test -f ${TOP}/bsdiff/README.md; then
+    git submodule update --init
+fi
+
 echo "Archiving ${PKG_VER} at ${GITREV} to ${TARFILE_TMP}"
 (cd ${TOP}; git archive --format=tar --prefix=${PKG_VER}/ ${GITREV}) > ${TARFILE_TMP}
 ls -al ${TARFILE_TMP}
