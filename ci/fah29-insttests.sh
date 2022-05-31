aea20704b5b7a246cef3358662b4ae6e02763b92
Colin Walters <walters@verbum.org>
2022-05-31 10:49:09 -0400

    ci: Drop an unused files
    Cruft from previous CI flows.

--- ci/fah29-insttests.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-set -xeuo pipefail
-
-./tests/installed/provision.sh
-# TODO: enhance papr to have caching, a bit like https://docs.travis-ci.com/user/caching/
-cd tests/installed
-curl -Lo fedora-atomic-host.qcow2 https://getfedora.org/atomic_qcow2_latest
-exec env "TEST_SUBJECTS=$(pwd)/fedora-atomic-host.qcow2" ./run.sh
