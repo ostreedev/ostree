23446a0218d4d3997a184448cd6708bdbb5e74ac
Colin Walters <walters@verbum.org>
2022-10-31 09:00:28 -0400

    Remove readdir-rand
    This was only there to reproduce a bug we hit long ago
    with bootloader file ordering.  We're extremely unlikely
    to reintroduce such a bug, and it's not worth carrying around
    this code.

--- tests/libtest.sh
+++ tests/libtest.sh
@@ -172,17 +172,9 @@ if test -n "${ASAN_OPTIONS:-}"; then
     BUILT_WITH_ASAN=1
 fi
 
+CMD_PREFIX=""
 if test -n "${OT_TESTS_VALGRIND:-}"; then
     CMD_PREFIX="env G_SLICE=always-malloc OSTREE_SUPPRESS_SYNCFS=1 valgrind -q --error-exitcode=1 --leak-check=full --num-callers=30 --suppressions=${test_srcdir}/glib.supp --suppressions=${test_srcdir}/ostree.supp"
-else
-    # In some cases the LD_PRELOAD may cause obscure problems,
-    # e.g. right now it breaks for me with -fsanitize=address, so
-    # let's allow users to skip it.
-    if test -z "${OT_SKIP_READDIR_RAND:-}" && test -z "${BUILT_WITH_ASAN:-}"; then
-	CMD_PREFIX="env LD_PRELOAD=${test_builddir}/libreaddir-rand.so"
-    else
-	CMD_PREFIX=""
-    fi
 fi
 
 if test -n "${OSTREE_UNINSTALLED:-}"; then
