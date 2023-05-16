c6ed5cc7b2e09730fda8d7c0b2502774796020e8
Alexander Larsson <alexl@redhat.com>
2023-05-16 16:01:33 +0200

    fsverity: Add _ostree_fsverity_sign helper
    This code signs a fsverity digest (using openssl) such that the
    resulting signature can be used with the FS_IOC_ENABLE_VERITY ioctl.

--- src/libostree/ostree-repo-private.h
+++ src/libostree/ostree-repo-private.h
@@ -393,6 +393,9 @@ gboolean _ostree_tmpf_fsverity_core (GLnxTmpfile *tmpf, _OstreeFeatureSupport fs
                                      gboolean *supported, GError **error);
 
 gboolean _ostree_tmpf_fsverity (OstreeRepo *self, GLnxTmpfile *tmpf, GError **error);
+gboolean _ostree_fsverity_sign (const char *certfile, const char *keyfile,
+                                const guchar *fsverity_digest, GBytes **data_out,
+                                GCancellable *cancellable, GError **error);
 
 gboolean _ostree_repo_verify_bindings (const char *collection_id, const char *ref_name,
                                        GVariant *commit, GError **error);
