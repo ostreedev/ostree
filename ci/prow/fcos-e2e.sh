6df69880de0399a22aa98f549466a30961331d3d
Colin Walters <walters@verbum.org>
2023-06-19 19:07:01 -0400

    ci: Sync prow config with rpm-ostree, enable composefs there
    I want to gain testing over the composefs path; but without
    yet changing the main Jenkins build.  Because we have duplicate/overlapping
    CI systems, we can take advantage of this by testing the composefs
    flow via Prow.
    
    Sync the Prow build scripts with what's in coreos/rpm-ostree
    right now.
    
    Then let's test the composefs flow a bit more e2e there.

---
+++ ci/prow/fcos-e2e.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -xeuo pipefail
+
+# Prow jobs don't support adding emptydir today
+export COSA_SKIP_OVERLAY=1
+# And suppress depcheck since we didn't install via RPM
+export COSA_SUPPRESS_DEPCHECK=1
+ostree --version
+cd $(mktemp -d)
+cosa init https://github.com/coreos/fedora-coreos-config/
+rsync -rlv /cosa/component-install/ overrides/rootfs/
+cosa fetch
+cosa build
+# For now, Prow just runs the composefs tests, since Jenkins covers the others
+cosa kola run 'ext.ostree.destructive-rs.composefs*'
