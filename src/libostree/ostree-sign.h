ef44e33def4dd8dd9d13f6331b0eeab37960f535
Daiki Ueno <dueno@redhat.com>
2024-08-11 15:08:12 +0900

    sign: Factor out logic to read key blobs
    This defines a new interface OstreeBlobReader, which encapsulates the
    key file parsing logic. This would make it easy to support custom file
    formats such as PEM.
    
    Signed-off-by: Daiki Ueno <dueno@redhat.com>

--- src/libostree/ostree-sign.h
+++ src/libostree/ostree-sign.h
@@ -27,6 +27,7 @@
 #include <glib-object.h>
 #include <glib.h>
 
+#include "ostree-blob-reader.h"
 #include "ostree-ref.h"
 #include "ostree-remote.h"
 #include "ostree-types.h"
@@ -113,4 +114,11 @@ OstreeSign *ostree_sign_get_by_name (const gchar *name, GError **error);
 _OSTREE_PUBLIC
 gboolean ostree_sign_summary (OstreeSign *self, OstreeRepo *repo, GVariant *keys,
                               GCancellable *cancellable, GError **error);
+
+_OSTREE_PUBLIC
+OstreeBlobReader *ostree_sign_read_pk (OstreeSign *self, GInputStream *stream);
+
+_OSTREE_PUBLIC
+OstreeBlobReader *ostree_sign_read_sk (OstreeSign *self, GInputStream *stream);
+
 G_END_DECLS
