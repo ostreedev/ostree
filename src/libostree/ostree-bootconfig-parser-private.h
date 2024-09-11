08487091256b93493f8d692e37ab3d892c758da1
Igor Opaniuk <igor.opaniuk@foundries.io>
2024-09-11 18:03:10 +0200

    sysroot: Support boot counting for boot entries
    Add support for boot counting for bootloader entries [1].
    The boot counting data is stored in the name of the boot loader entry.
    A boot loader entry file name may contain a plus (+) followed by a number.
    This may optionally be followed by a minus (-) followed by a second number.
    The dot (.) and file name suffix (conf or efi) must immediately follow.
    
    The feature is enabled via sysroot configuration:
    [sysroot]
    boot-counting-tries=3
    
    Testing:
    $ ostree admin deploy 91fc19319be9e79d07159303dff125f40f10e5c25614630dcbed23d95e36f907
    Copying /etc changes: 2 modified, 3 removed, 4 added
    bootfs is sufficient for calculated new size: 0 bytes
    Transaction complete; bootconfig swap: yes; bootversion: boot.0.1, deployment count change: 1
    
    $ ls /boot/loader/entries
    ostree-1.conf  ostree-2+3.conf
    
    [1] https://uapi-group.org/specifications/specs/boot_loader_specification/#boot-counting
    Signed-off-by: Igor Opaniuk <igor.opaniuk@foundries.io>
    Signed-off-by: Colin Walters <walters@verbum.org>

---
+++ src/libostree/ostree-bootconfig-parser-private.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: LGPL-2.0+ */
+
+#pragma once
+
+#include "ostree-bootconfig-parser.h"
+
+G_BEGIN_DECLS
+
+const char *_ostree_bootconfig_parser_filename (OstreeBootconfigParser *self);
+
+G_END_DECLS
