d4808dadabacf3c038668f093be4861d242e7315
Samuel Zeter <samuelzeter@gmail.com>
2025-05-20 14:41:12 +1000

    tests/inst: convert more sh-inline to xshell

--- tests/inst/src/repobin.rs
+++ tests/inst/src/repobin.rs
@@ -5,23 +5,23 @@ use std::path::Path;
 
 use crate::test::*;
 use anyhow::{Context, Result};
-use sh_inline::{bash, bash_command};
+use sh_inline::bash_command;
 use with_procspawn_tempdir::with_procspawn_tempdir;
+use xshell::cmd;
 
 pub(crate) fn itest_basic() -> Result<()> {
-    bash!(r"ostree --help >/dev/null")?;
+    let sh = xshell::Shell::new()?;
+    cmd!(sh, "ostree --help >/dev/null").run()?;
     Ok(())
 }
 
 #[with_procspawn_tempdir]
 pub(crate) fn itest_nofifo() -> Result<()> {
+    let sh = xshell::Shell::new()?;
     assert!(std::path::Path::new(".procspawn-tmpdir").exists());
-    bash!(
-        r"ostree --repo=repo init --mode=archive
-    mkdir tmproot
-    mkfifo tmproot/afile
-"
-    )?;
+    cmd!(sh, "ostree --repo=repo init --mode=archive").run()?;
+    cmd!(sh, "mkdir tmproot").run()?;
+    cmd!(sh, "mkfifo tmproot/afile").run()?;
     cmd_fails_with(
         bash_command!(
             r#"ostree --repo=repo commit -b fifotest -s "commit fifo" --tree=dir=./tmproot"#
@@ -34,23 +34,30 @@ pub(crate) fn itest_nofifo() -> Result<()> {
 
 #[with_procspawn_tempdir]
 pub(crate) fn itest_mtime() -> Result<()> {
-    bash!(
-        r"ostree --repo=repo init --mode=archive
-    mkdir tmproot
-    echo afile > tmproot/afile
-    ostree --repo=repo commit -b test --tree=dir=tmproot >/dev/null
-"
-    )?;
+    let sh = xshell::Shell::new()?;
+    cmd!(sh, "ostree --repo=repo init --mode=archive").run()?;
+    cmd!(sh, "mkdir tmproot").run()?;
+    cmd!(sh, "echo afile > tmproot/afile").run()?;
+    cmd!(
+        sh,
+        "ostree --repo=repo commit -b test --tree=dir=tmproot >/dev/null"
+    )
+    .run()?;
     let ts = Path::new("repo").metadata()?.modified().unwrap();
     std::thread::sleep(std::time::Duration::from_secs(1));
-    bash!(r#"ostree --repo=repo commit -b test -s "bump mtime" --tree=dir=tmproot >/dev/null"#)?;
+    cmd!(
+        sh,
+        "ostree --repo=repo commit -b test -s 'bump mtime' --tree=dir=tmproot >/dev/null"
+    )
+    .run()?;
     assert_ne!(ts, Path::new("repo").metadata()?.modified().unwrap());
     Ok(())
 }
 
 #[with_procspawn_tempdir]
 pub(crate) fn itest_extensions() -> Result<()> {
-    bash!(r"ostree --repo=repo init --mode=bare")?;
+    let sh = xshell::Shell::new()?;
+    cmd!(sh, "ostree --repo=repo init --mode=bare").run()?;
     assert!(Path::new("repo/extensions").exists());
     Ok(())
 }
@@ -62,6 +69,7 @@ pub(crate) fn itest_pull_basicauth() -> Result<()> {
         ..Default::default()
     };
     let serverrepo = Path::new("server/repo");
+    let sh = xshell::Shell::new()?;
     std::fs::create_dir_all(&serverrepo)?;
     with_webserver_in(&serverrepo, &opts, move |addr| {
         let baseuri = http::Uri::from_maybe_shared(format!("http://{}/", addr).into_bytes())?;
@@ -72,22 +80,35 @@ pub(crate) fn itest_pull_basicauth() -> Result<()> {
         )?;
         let osroot = Path::new("osroot");
         crate::treegen::mkroot(&osroot)?;
-        bash!(
-            r#"ostree --repo=${serverrepo} init --mode=archive
-        ostree --repo=${serverrepo} commit -b os --tree=dir=${osroot} >/dev/null
-        mkdir client
-        cd client
-        ostree --repo=repo init --mode=archive
-        ostree --repo=repo remote add --set=gpg-verify=false origin-unauth ${baseuri}
-        ostree --repo=repo remote add --set=gpg-verify=false origin-badauth ${unauthuri}
-        ostree --repo=repo remote add --set=gpg-verify=false origin-goodauth ${authuri}
-        "#,
-            osroot = osroot,
-            serverrepo = serverrepo,
-            baseuri = baseuri.to_string(),
-            unauthuri = unauthuri.to_string(),
-            authuri = authuri.to_string()
-        )?;
+        cmd!(sh, "ostree --repo={serverrepo} init --mode=archive").run()?;
+        cmd!(
+            sh,
+            "ostree --repo={serverrepo} commit -b os --tree=dir={osroot} >/dev/null"
+        )
+        .run()?;
+        let baseuri_s = baseuri.to_string();
+        let unauthuri_s = unauthuri.to_string();
+        let authuri_s = authuri.to_string();
+        let basedir = sh.current_dir();
+        let dir = sh.create_dir("client")?;
+        let _p = sh.push_dir(dir);
+        cmd!(sh, "ostree --repo=repo init --mode=archive").run()?;
+        cmd!(
+            sh,
+            "ostree --repo=repo remote add --set=gpg-verify=false origin-unauth {baseuri_s}"
+        )
+        .run()?;
+        cmd!(
+            sh,
+            "ostree --repo=repo remote add --set=gpg-verify=false origin-badauth {unauthuri_s}"
+        )
+        .run()?;
+        cmd!(
+            sh,
+            "ostree --repo=repo remote add --set=gpg-verify=false origin-goodauth {authuri_s}"
+        )
+        .run()?;
+
         for rem in &["unauth", "badauth"] {
             cmd_fails_with(
                 bash_command!(
@@ -99,7 +120,10 @@ pub(crate) fn itest_pull_basicauth() -> Result<()> {
             )
             .context(rem)?;
         }
-        bash!(r#"ostree --repo=client/repo pull origin-goodauth os >/dev/null"#,)?;
+        let _p = sh.push_dir(basedir);
+        cmd!(sh, "ostree --repo=client/repo pull origin-goodauth os")
+            .ignore_stdout()
+            .run()?;
         Ok(())
     })?;
     Ok(())
